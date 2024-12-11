a140f0a3746b4b9f7edb598e8a668caf0e1566a4
milkzee <68483089+milkzee@users.noreply.github.com>
2024-12-11 23:30:07 +0200

    Initial commit

---
+++ задание 1.1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void DetermineSeason()
+    {
+        Console.Write("Введите номер месяца: ");
+        string input = Console.ReadLine();
+
+        if (!int.TryParse(input, out int month))
+        {
+            Console.WriteLine("Это не число");
+            return;
+        }
+
+        if (month < 1 || month > 12)
+        {
+            Console.WriteLine("Это не номер месяца");
+        }
+        else if (month == 12 || month == 1 || month == 2)
+        {
+            Console.WriteLine("Зима");
+        }
+        else if (month >= 3 && month <= 5)
+        {
+            Console.WriteLine("Весна");
+        }
+        else if (month >= 6 && month <= 8)
+        {
+            Console.WriteLine("Лето");
+        }
+        else if (month >= 9 && month <= 11)
+        {
+            Console.WriteLine("Осень");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        DetermineSeason();
+    }
+}
